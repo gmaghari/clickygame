bf20504d501fb70bcb8391669e91d07a18c330c9
Garry Maghari <garry.maghari@gmail.com>
2019-01-27 22:09:38 -0800

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ import CardContainer from "./components/CardContainer"
 class App extends Component {
     // setting the state
     state = {
-      count: 0,
-      topScore: 0
+        count: 0,
+        topScore: 0
     }
-  
+
     // update score on navbar
     updateCurrentScore = (newCount) => {
-      this.setState({count: newCount});
+        this.setState({count: newCount});
     }
-  
+
     // update top score in the navbar
     updateTopScore = (newTop) => {
-      // update new top score
-      if (newTop > this.state.topScore) {
+        // update new top score
+        if (newTop > this.state.topScore) {
         this.setState({topScore: newTop - 1})
-      }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            <Navbar score={this.state.count} top={this.state.topScore}/>
+            <Header/>
+            <CardContainer updateCurrentScore={this.updateCurrentScore} updateTopScore={this.updateTopScore}/>
+            </div>
+        );
+        }
     }
 
 export default App;
